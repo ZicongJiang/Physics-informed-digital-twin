5142113c58efd5e0efdb190cacfe67188b75a7e7
Zicong Jiang <75415960+ZicongJiang@users.noreply.github.com>
2026-05-08 08:16:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --N_data 1000 --Nitr 80000 --nsym 128 --sps 2 --step_num 800 --step_num_dt 4 --power_dbm 0 --lamio 1 --fft_flag 0 --loss l1 --noise_db 20
+python main.py --N_data 1000 --Nitr 80000 --nsym 128 --sps 2 --step_num 800 --step_num_dt 4 --power_dbm 0 --lamio 1 --loss l1 --noise_db 20
